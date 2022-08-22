54f04dce94055620cef25ee162489d8bbbab7530
noumanshah436 <noumanshah436@gmail.com>
2022-08-23 00:41:40 +0500

    Deleting Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -54,6 +54,13 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
 
   private
     # this method is used to get Posted parameters of form
