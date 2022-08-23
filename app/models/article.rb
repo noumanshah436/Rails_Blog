38e53931f2594dfbad10a227cb1e63a92f1ab908
noumanshah436 <noumanshah436@gmail.com>
2022-08-23 11:10:26 +0500

    Associating Models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  has_many :comments
   #  this will ensure that all articles have a title that is at least five characters long
   validates :title, presence: true, length: { minimum: 5 }
 end
